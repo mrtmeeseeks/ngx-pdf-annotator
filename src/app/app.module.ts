a8fcb0abc34c4916cc1b7d51b26d3fa222ee2398
taulant.disha <tao.disha@gmail.com>
2020-04-11 20:41:19 +0300

    Docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PdfAnnotatorModule } from './pdf-annotator/pdf-annotator.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, PdfAnnotatorModule, BrowserAnimationsModule],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue : '/pdf/'}],
   bootstrap: [AppComponent]
 })
 export class AppModule {}

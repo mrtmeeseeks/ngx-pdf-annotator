c951a41c36d2d12437f596a8e9d338341edbee43
taulant.disha <tao.disha@gmail.com>
2020-04-11 20:35:42 +0300

    Docs

--- src/app/pdf-annotator/pdf-annotator.component.ts
+++ src/app/pdf-annotator/pdf-annotator.component.ts
@@ -46,7 +46,7 @@ export class PdfAnnotatorComponent implements OnInit {
     const annotationRenderer = new PdfAnnotationRenderer(this._renderer);
 
     this.renderOptions = new RenderOptions(
-      'assets/rest2.pdf',
+      'rest2.pdf',
       this._el.nativeElement,
       annotationRenderer
     );
